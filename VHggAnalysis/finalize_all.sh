b0e0ac53960fa9cd4c8b7df3d3dabc057471f8a9
pandolf <>
2012-08-26 16:48:22 +0000

    new dataset organization

--- VHggAnalysis/finalize_all.sh
+++ VHggAnalysis/finalize_all.sh
@@ -1,13 +1,11 @@
 # usage: source finalize_all.sh [selectionType]
 
 
- ./finalize_VHgg  GluGluToHToGG_M-125_8TeV-powheg-pythia6 $1
- ./finalize_VHgg  WH_ZH_HToGG_M-125_8TeV-pythia6 $1
+ ./finalize_VHgg  HToGG_M-125_8TeV-pythia6 $1
  ./finalize_VHgg  DiPhotonBox_8TeV-pythia6 $1
  ./finalize_VHgg  V_8TeV $1
  ./finalize_VHgg  GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
  ./finalize_VHgg  QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
  ./finalize_VHgg  TT_8TeV $1
- ./finalize_VHgg  VG_8TeV $1
  ./finalize_VHgg  VV_8TeV $1
  ./finalize_VHgg  VGG_8TeV $1
