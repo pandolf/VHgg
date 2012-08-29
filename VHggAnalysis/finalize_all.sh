2546f27a5d1042680d9bc0d3c5bf9ba4d20f82eb
pandolf <>
2012-08-29 14:14:47 +0000

    little trick to be able to compile whil running

--- VHggAnalysis/finalize_all.sh
+++ VHggAnalysis/finalize_all.sh
@@ -1,11 +1,14 @@
 # usage: source finalize_all.sh [selectionType]
 
 
- ./finalize_VHgg  HToGG_M-125_8TeV-pythia6 $1
- ./finalize_VHgg  DiPhoton_8TeV-pythia6 $1
- ./finalize_VHgg  V_8TeV $1
- ./finalize_VHgg  GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
- ./finalize_VHgg  QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
- ./finalize_VHgg  TT_8TeV $1
- ./finalize_VHgg  VV_8TeV $1
- ./finalize_VHgg  VGG_8TeV $1
+ cp finalize_VHgg finalize_VHgg_tmp
+ ./finalize_VHgg_tmp  HToGG_M-125_8TeV-pythia6 $1
+ ./finalize_VHgg_tmp  WH_ZH_HToGG_M-125_8TeV-pythia6 $1
+ ./finalize_VHgg_tmp  DiPhoton_8TeV-pythia6 $1
+ ./finalize_VHgg_tmp  V_8TeV $1
+ ./finalize_VHgg_tmp  GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
+ ./finalize_VHgg_tmp  QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
+ ./finalize_VHgg_tmp  TT_8TeV $1
+ ./finalize_VHgg_tmp  VV_8TeV $1
+ ./finalize_VHgg_tmp  VGG_8TeV $1
+ rm finalize_VHgg_tmp
