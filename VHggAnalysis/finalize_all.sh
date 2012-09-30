ee73c15cc583497d6d33d1efe2329deed08fe254
pandolf <>
2012-09-30 16:06:35 +0000

    introducing redntpdir mechanism

--- VHggAnalysis/finalize_all.sh
+++ VHggAnalysis/finalize_all.sh
@@ -1,14 +1,13 @@
-# usage: source finalize_all.sh [selectionType]
+# usage: source finalize_all.sh [redntpProdVersion] [selectionType]
 
 
  cp finalize_VHgg finalize_VHgg_tmp
- ./finalize_VHgg_tmp  HToGG_M-125_8TeV-pythia6 $1
- ./finalize_VHgg_tmp  WH_ZH_HToGG_M-125_8TeV-pythia6 $1
- ./finalize_VHgg_tmp  DiPhoton_8TeV-pythia6 $1
- ./finalize_VHgg_tmp  V_8TeV $1
- ./finalize_VHgg_tmp  GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
- ./finalize_VHgg_tmp  QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $1
- ./finalize_VHgg_tmp  TT_8TeV $1
- ./finalize_VHgg_tmp  VV_8TeV $1
- ./finalize_VHgg_tmp  VGG_8TeV $1
+ ./finalize_VHgg_tmp  $1 HToGG_M-125_8TeV-pythia6 $2
+ ./finalize_VHgg_tmp  $1 DiPhoton_8TeV-pythia6 $2
+ ./finalize_VHgg_tmp  $1 V_8TeV $2
+ ./finalize_VHgg_tmp  $1 GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
+ ./finalize_VHgg_tmp  $1 QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
+ ./finalize_VHgg_tmp  $1 TT_8TeV $2
+ ./finalize_VHgg_tmp  $1 VV_8TeV $2
+ ./finalize_VHgg_tmp  $1 VGG_8TeV $2
  rm finalize_VHgg_tmp
