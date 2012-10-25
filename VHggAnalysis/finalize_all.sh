f8f5a94d5d2967b899d10601e0a9d9ffe5f5d2cb
pandolf <>
2012-10-25 17:02:44 +0000

    moved to TTVH

--- VHggAnalysis/finalize_all.sh
+++ VHggAnalysis/finalize_all.sh
@@ -1,13 +1,14 @@
 # usage: source finalize_all.sh [redntpProdVersion] [selectionType]
 
 
- cp finalize_VHgg finalize_VHgg_tmp
- ./finalize_VHgg_tmp  $1 HToGG_M-125_8TeV-pythia6 $2
- ./finalize_VHgg_tmp  $1 DiPhoton_8TeV-pythia6 $2
- ./finalize_VHgg_tmp  $1 V_8TeV $2
- ./finalize_VHgg_tmp  $1 GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
- ./finalize_VHgg_tmp  $1 QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
- ./finalize_VHgg_tmp  $1 TT_8TeV $2
- ./finalize_VHgg_tmp  $1 VV_8TeV $2
- ./finalize_VHgg_tmp  $1 VGG_8TeV $2
- rm finalize_VHgg_tmp
+ cp finalize_TTVHgg finalize_TTVHgg_tmp
+ ./finalize_TTVHgg_tmp  $1 DATA_Run2012ABC $2
+ ./finalize_TTVHgg_tmp  $1 HToGG_M-125_8TeV-pythia6 $2
+ ./finalize_TTVHgg_tmp  $1 DiPhoton_8TeV-pythia6 $2
+ ./finalize_TTVHgg_tmp  $1 V_8TeV $2
+ ./finalize_TTVHgg_tmp  $1 GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
+ ./finalize_TTVHgg_tmp  $1 QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
+ ./finalize_TTVHgg_tmp  $1 TT_8TeV $2
+ ./finalize_TTVHgg_tmp  $1 VV_8TeV $2
+ ./finalize_TTVHgg_tmp  $1 VGG_8TeV $2
+ rm finalize_TTVHgg_tmp
