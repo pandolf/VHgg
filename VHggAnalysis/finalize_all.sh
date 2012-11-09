f224df9702d7d9b8f8032abe2ebf7d0865cc273f
micheli <>
2012-11-09 17:01:22 +0000

    new parameters added

--- VHggAnalysis/finalize_all.sh
+++ VHggAnalysis/finalize_all.sh
@@ -1,14 +1,14 @@
-# usage: source finalize_all.sh [redntpProdVersion] [selectionType]
+# usage: source finalize_all.sh [redntpProdVersion] [selectionType] [use_inverted_photID=\"false\"] [bTaggerType=\"JP\"]"
 
 
  cp finalize_TTVHgg finalize_TTVHgg_tmp
- ./finalize_TTVHgg_tmp  $1 DATA_Run2012ABC $2
- ./finalize_TTVHgg_tmp  $1 HToGG_M-125_8TeV-pythia6 $2
- ./finalize_TTVHgg_tmp  $1 DiPhoton_8TeV-pythia6 $2
- ./finalize_TTVHgg_tmp  $1 V_8TeV $2
- ./finalize_TTVHgg_tmp  $1 GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
- ./finalize_TTVHgg_tmp  $1 QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2
- ./finalize_TTVHgg_tmp  $1 TT_8TeV $2
- ./finalize_TTVHgg_tmp  $1 VV_8TeV $2
- ./finalize_TTVHgg_tmp  $1 VGG_8TeV $2
+ ./finalize_TTVHgg_tmp  $1 DATA_Run2012ABC $2 $3
+ ./finalize_TTVHgg_tmp  $1 HToGG_M-125_8TeV-pythia6 $2 $3
+ ./finalize_TTVHgg_tmp  $1 DiPhoton_8TeV-pythia6 $2 $3
+ ./finalize_TTVHgg_tmp  $1 V_8TeV $2 $3
+ ./finalize_TTVHgg_tmp  $1 GJet_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2 $3
+ ./finalize_TTVHgg_tmp  $1 QCD_doubleEMEnriched_TuneZ2star_8TeV-pythia6 $2 $3
+ ./finalize_TTVHgg_tmp  $1 TT_8TeV $2 $3 
+ ./finalize_TTVHgg_tmp  $1 VV_8TeV $2 $3
+ ./finalize_TTVHgg_tmp  $1 VGG_8TeV $2 $3
  rm finalize_TTVHgg_tmp
